2214331f9149dff36bd957f370358b1ccb1f9542
Alexander Juda <alexanderjuda@gmail.com>
2021-12-31 15:30:24 +0100

    Add todo list with datastructures & operations

--- src/hyperplane/v4/flat.py
+++ src/hyperplane/v4/flat.py
@@ -3,6 +3,27 @@ import typing as t
 from hyperplane.core import Coord
 
 
+# Data structures needed:
+# - [x] pt
+# - [x] hP
+# - [x] hPC
+# - [ ] Esum
+# - [ ] segment
+
+# Operations needed:
+# - [ ] hP contains pt?
+# - [ ] hPC contains pt?
+# - [ ] merge two Esum with union
+# - [ ] merge two Esum with intersection
+# - [ ] hP conjugate
+# - [ ] hPC conjugate
+# - [ ] Esum conjugate
+# - [ ] select intersecting segments
+
+# Plotting:
+# - [ ] point-by-point test
+
+
 class Pt(t.NamedTuple):
     x: Coord
     y: Coord
