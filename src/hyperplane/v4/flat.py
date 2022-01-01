2985b2c7b2e6d1a9255b90a4ad1fec4c924a554d
Alexander Juda <alexanderjuda@gmail.com>
2022-01-01 14:52:37 +0100

    Implement  \in Hpc$ check

--- src/hyperplane/v4/flat.py
+++ src/hyperplane/v4/flat.py
@@ -14,7 +14,7 @@ from hyperplane.core import Coord
 
 # Operations needed:
 # - [x] hP contains pt?
-# - [ ] hPC contains pt?
+# - [x] hPC contains pt?
 # - [ ] merge two Esum with union
 # - [ ] merge two Esum with intersection
 # - [ ] hP conjugate
@@ -31,7 +31,8 @@ class Pt(t.NamedTuple):
     y: Coord
 
     @property
-    def vector(self) -> np.ndarray:
+    def position(self) -> np.ndarray:
+        """3d position vector."""
         return np.array([*self, 0])
 
 
@@ -39,19 +40,28 @@ class Hp(t.NamedTuple):
     p1: Pt
     p2: Pt
 
+    def contains(self, point: Pt) -> bool:
+        return _z_factor(self, point) < 0
+
 
 class Hpc(t.NamedTuple):
     p1: Pt
     p2: Pt
 
+    def contains(self, point: Pt) -> bool:
+        return _z_factor(self, point) >= 0
+
 
 Hs = t.Union[Hp, Hpc]
 
 
-def contains_point(hs: Hs, pt: Pt) -> bool:
-    p1, p2 = [p.vector for p in hs]
+def _z_factor(half_space: Hs, point: Pt) -> float:
+    """Z coordinate of the cross product between the halfspace's vector and the
+    position vector of the tested point.
+    """
+    p1, p2 = [p.position for p in half_space]
     v_hs = p2 - p1
-    v_test = pt.vector
+    v_test = point.position
     v_cross = np.cross(v_hs, v_test)
 
-    return v_cross[-1] < 0
+    return v_cross[-1]
--- tests/hyperplane/v4/test_flat.py
+++ tests/hyperplane/v4/test_flat.py
@@ -1,30 +1,58 @@
 from hyperplane.v4 import flat
 import pytest
 
+HP_EXAMPLES = [
+    # --- vertical line ---
+    (flat.Pt(0, 1), flat.Pt(0, 4), flat.Pt(1, 2)),
+    # point outside the segment boundary
+    (flat.Pt(0, 1), flat.Pt(0, 4), flat.Pt(1, 6)),
+    (flat.Pt(0, 1), flat.Pt(0, 4), flat.Pt(1, -1)),
+    # --- horizontal line ---
+    (flat.Pt(1, 1), flat.Pt(4, 1), flat.Pt(2, -2)),
+    # point on segment
+    # (flat.Pt(1, 1), flat.Pt(4, 1), flat.Pt(2, 1)),
+    # --- problematic samples ---
+    #     B
+    #  A     C
+    # hp(AB) -> y <= x -> -x + y <= 0
+    (flat.Pt(0, 0), flat.Pt(5, 5), flat.Pt(10, 0)),
+]
+
+
+HPC_EXAMPLES = [
+    # --- vertical line ---
+    (flat.Pt(0, 1), flat.Pt(0, 4), flat.Pt(-1, 2)),
+    (flat.Pt(0, 1), flat.Pt(0, 4), flat.Pt(-1, -3)),
+    (flat.Pt(0, 1), flat.Pt(0, 4), flat.Pt(-1, 7)),
+    # point on segment
+    (flat.Pt(0, 1), flat.Pt(0, 4), flat.Pt(0, 2)),
+    # segment vertices
+    (flat.Pt(0, 1), flat.Pt(0, 4), flat.Pt(0, 1)),
+    (flat.Pt(0, 1), flat.Pt(0, 4), flat.Pt(0, 4)),
+    # --- horizontal line ---
+    (flat.Pt(1, 1), flat.Pt(4, 1), flat.Pt(2, 2)),
+    (flat.Pt(1, 1), flat.Pt(4, 1), flat.Pt(0, 2)),
+    (flat.Pt(1, 1), flat.Pt(4, 1), flat.Pt(7, 2)),
+    # --- top-right line ---
+    (flat.Pt(1, 1), flat.Pt(4, 5), flat.Pt(0, 1)),
+    (flat.Pt(1, 1), flat.Pt(4, 5), flat.Pt(0, 0)),
+    # --- top-left line ---
+    (flat.Pt(6, 1), flat.Pt(1, 5.5), flat.Pt(-0.01, 0)),
+]
+
 
 class TestHSContainsPt:
-    POSITIVE_EXAMPLES = [
-        # --- vertical line ---
-        (flat.Hp(flat.Pt(0, 1), flat.Pt(0, 4)), flat.Pt(1, 2)),
-        # point on segment
-        # (flat.Hp(flat.Pt(0, 1), flat.Pt(0, 4)), flat.Pt(0, 2)),
-        # point outside the segment boundary
-        (flat.Hp(flat.Pt(0, 1), flat.Pt(0, 4)), flat.Pt(1, 6)),
-        (flat.Hp(flat.Pt(0, 1), flat.Pt(0, 4)), flat.Pt(1, -1)),
-        # segment vertices
-        # (flat.Hp(flat.Pt(0, 1), flat.Pt(0, 4)), flat.Pt(0, 1)),
-        # (flat.Hp(flat.Pt(0, 1), flat.Pt(0, 4)), flat.Pt(0, 4)),
-        # --- horizontal line ---
-        (flat.Hp(flat.Pt(1, 1), flat.Pt(4, 1)), flat.Pt(2, -2)),
-        # point on segment
-        # (flat.Hp(flat.Pt(1, 1), flat.Pt(4, 1)), flat.Pt(2, 1)),
-        # --- problematic samples ---
-        #     B
-        #  A     C
-        # hp(AB) -> y <= x -> -x + y <= 0
-        (flat.Hp(flat.Pt(0, 0), flat.Pt(5, 5)), flat.Pt(10, 0)),
-    ]
+    @pytest.mark.parametrize("p1,p2,tested_point", HP_EXAMPLES)
+    def test_hp_contains_point(self, p1, p2, tested_point):
+        assert flat.Hp(p1, p2).contains(tested_point)
 
-    @pytest.mark.parametrize("hs,point", POSITIVE_EXAMPLES)
-    def test_positive(self, hs, point):
-        assert flat.contains_point(hs, point)
+    @pytest.mark.parametrize("p1,p2,tested_point", HPC_EXAMPLES)
+    def test_hpc_contains_point(self, p1, p2, tested_point):
+        assert flat.Hpc(p1, p2).contains(tested_point)
+
+    @pytest.mark.parametrize("p1,p2,tested_point", [*HP_EXAMPLES, *HPC_EXAMPLES])
+    def test_point_is_either_in_hp_or_hpc(self, p1, p2, tested_point):
+        hp = flat.Hp(p1, p2)
+        hpc = flat.Hpc(p1, p2)
+
+        assert hp.contains(tested_point) != hpc.contains(tested_point)
