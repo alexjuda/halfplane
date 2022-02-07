3e3f8a76d50edd61f9b03227ad4630a0d6869168
Alexander Juda <alexanderjuda@gmail.com>
2022-02-07 10:50:26 +0100

    Plot letter c

--- src/hyperplane/run/dump_samples.py
+++ src/hyperplane/run/dump_samples.py
@@ -1,14 +1,102 @@
 from pathlib import Path
 
-import matplotlib.pyplot as plt
-
 from ..flat import Esum, Pt, Hp, Hpc
 from .. import flat
 from .. import io
 from .. import plots
 
 
-def _make_shapes():
+def _make_c():
+    # letter C:
+    #
+    #      -------
+    #     /      |
+    #    /   -----
+    #   /   /
+    #   |  /
+    #   |  |
+    #   |  \
+    #   \   \
+    #    \   -----
+    #     \      |
+    #      -------
+    external = Esum(
+        {
+            frozenset(
+                [
+                    # vertical line (|)
+                    Hpc(
+                        Pt(2, 10),
+                        Pt(2, 0),
+                    ),
+                    # diagonal line (/)
+                    Hpc(
+                        Pt(6, 14),
+                        Pt(2, 10),
+                    ),
+                    # horizontal line (-)
+                    Hpc(
+                        Pt(10, 14),
+                        Pt(6, 14),
+                    ),
+                    # vertical line (|)
+                    Hpc(
+                        Pt(12, 10),
+                        Pt(12, 14),
+                    ),
+                    # diagonal line (\)
+                    Hpc(
+                        Pt(2, 6),
+                        Pt(6, 2),
+                    ),
+                    # horizontal line (-)
+                    Hpc(
+                        Pt(6, 2),
+                        Pt(10, 2),
+                    ),
+                ]
+            )
+        }
+    )
+
+    internal = Esum(
+        {
+            frozenset(
+                [
+                    # vertical line (|)
+                    Hpc(
+                        Pt(4, 10),
+                        Pt(4, 0),
+                    ),
+                    # diagonal line (/)
+                    Hpc(
+                        Pt(8, 14),
+                        Pt(4, 10),
+                    ),
+                    # horizontal line (-)
+                    Hpc(
+                        Pt(10, 12),
+                        Pt(6, 12),
+                    ),
+                    # diagonal line (\)
+                    Hpc(
+                        Pt(4, 6),
+                        Pt(8, 2),
+                    ),
+                    # horizontal line (-)
+                    Hpc(
+                        Pt(6, 4),
+                        Pt(10, 4),
+                    ),
+                ]
+            )
+        }
+    )
+
+    return external.difference(internal)
+
+
+def _make_basic_shapes():
     triangle = Esum(
         {
             frozenset(
@@ -33,7 +121,7 @@ def _make_shapes():
         }
     )
 
-    square = Esum(
+    rect = Esum(
         {
             frozenset(
                 [
@@ -61,11 +149,13 @@ def _make_shapes():
             )
         }
     )
-
     return [
         triangle,
-        square,
-        square.difference(triangle),
+        rect,
+        rect.difference(triangle),
+        triangle.difference(rect),
+        rect.union(triangle),
+        rect.intersection(triangle),
     ]
 
 
@@ -86,7 +176,7 @@ def _plot(esum, vertices, path):
 def main():
     RESULTS_PATH.mkdir(exist_ok=True)
 
-    for shape_i, shape in enumerate(_make_shapes()):
+    for shape_i, shape in enumerate([*_make_basic_shapes(), _make_c()]):
         vertices = flat.find_vertices(esum=shape)
 
         with open(RESULTS_PATH / f"shape_{shape_i}.json", "w") as f:
