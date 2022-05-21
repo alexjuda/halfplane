828b154e74851a0d9e0e7ae9d845f1f054f11f40
Alexander Juda <alexanderjuda@gmail.com>
2022-05-21 22:39:34 +0200

    Add crude c test

--- tests/hyperplane/test_regression.py
+++ tests/hyperplane/test_regression.py
@@ -39,10 +39,192 @@ def _triangle_boundary():
     ]
 
 
+def _crude_c_boundary():
+    return [
+        XSegment(
+            x1=X(
+                hs1=Hpc(
+                    p1=Pt(x=12, y=10),
+                    p2=Pt(x=-1, y=10),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=10, y=0),
+                    p2=Pt(x=10, y=11),
+                ),
+            ),
+            x2=X(
+                hs1=Hpc(
+                    p1=Pt(x=12, y=10),
+                    p2=Pt(x=-1, y=10),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=2, y=-1),
+                    p2=Pt(x=2, y=12),
+                ),
+            ),
+        ),
+        XSegment(
+            x1=X(
+                hs1=Hpc(
+                    p1=Pt(x=0, y=1),
+                    p2=Pt(x=11, y=1),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=10, y=0),
+                    p2=Pt(x=10, y=11),
+                ),
+            ),
+            x2=X(
+                hs1=Hpc(
+                    p1=Pt(x=10, y=0),
+                    p2=Pt(x=10, y=11),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=12, y=2),
+                    p2=Pt(x=-1, y=2),
+                ),
+            ),
+        ),
+        XSegment(
+            x1=X(
+                hs1=Hpc(
+                    p1=Pt(x=0, y=9),
+                    p2=Pt(x=11, y=9),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=10, y=0),
+                    p2=Pt(x=10, y=11),
+                ),
+            ),
+            x2=X(
+                hs1=Hpc(
+                    p1=Pt(x=12, y=10),
+                    p2=Pt(x=-1, y=10),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=10, y=0),
+                    p2=Pt(x=10, y=11),
+                ),
+            ),
+        ),
+        XSegment(
+            x1=X(
+                hs1=Hpc(
+                    p1=Pt(x=0, y=1),
+                    p2=Pt(x=11, y=1),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=2, y=-1),
+                    p2=Pt(x=2, y=12),
+                ),
+            ),
+            x2=X(
+                hs1=Hpc(
+                    p1=Pt(x=0, y=1),
+                    p2=Pt(x=11, y=1),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=10, y=0),
+                    p2=Pt(x=10, y=11),
+                ),
+            ),
+        ),
+        XSegment(
+            x1=X(
+                hs1=Hpc(
+                    p1=Pt(x=0, y=9),
+                    p2=Pt(x=11, y=9),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=2, y=-1),
+                    p2=Pt(x=2, y=12),
+                ),
+            ),
+            x2=X(
+                hs1=Hpc(
+                    p1=Pt(x=0, y=9),
+                    p2=Pt(x=11, y=9),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=10, y=0),
+                    p2=Pt(x=10, y=11),
+                ),
+            ),
+        ),
+        XSegment(
+            x1=X(
+                hs1=Hpc(
+                    p1=Pt(x=0, y=9),
+                    p2=Pt(x=11, y=9),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=1, y=11),
+                    p2=Pt(x=1, y=0),
+                ),
+            ),
+            x2=X(
+                hs1=Hpc(
+                    p1=Pt(x=12, y=2),
+                    p2=Pt(x=-1, y=2),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=1, y=11),
+                    p2=Pt(x=1, y=0),
+                ),
+            ),
+        ),
+        XSegment(
+            x1=X(
+                hs1=Hpc(
+                    p1=Pt(x=12, y=2),
+                    p2=Pt(x=-1, y=2),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=2, y=-1),
+                    p2=Pt(x=2, y=12),
+                ),
+            ),
+            x2=X(
+                hs1=Hpc(
+                    p1=Pt(x=0, y=9),
+                    p2=Pt(x=11, y=9),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=2, y=-1),
+                    p2=Pt(x=2, y=12),
+                ),
+            ),
+        ),
+        XSegment(
+            x1=X(
+                hs1=Hpc(
+                    p1=Pt(x=10, y=0),
+                    p2=Pt(x=10, y=11),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=12, y=2),
+                    p2=Pt(x=-1, y=2),
+                ),
+            ),
+            x2=X(
+                hs1=Hpc(
+                    p1=Pt(x=12, y=2),
+                    p2=Pt(x=-1, y=2),
+                ),
+                hs2=Hpc(
+                    p1=Pt(x=2, y=-1),
+                    p2=Pt(x=2, y=12),
+                ),
+            ),
+        ),
+    ]
+
+
 @pytest.mark.parametrize(
     "esum,expected_segments",
     [
         (common_shapes.triangle(), _triangle_boundary()),
+        (common_shapes.crude_c(), _crude_c_boundary()),
     ],
 )
 def test_detect_boundary(esum, expected_segments):
