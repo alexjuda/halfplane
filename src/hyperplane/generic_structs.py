30ed5d5642d9540de9ea2b3a6111a64935daefbf
Alexander Juda <alexanderjuda@gmail.com>
2022-05-22 22:39:00 +0200

    Make FOSet support element type with mypy

--- src/hyperplane/generic_structs.py
+++ src/hyperplane/generic_structs.py
@@ -3,21 +3,27 @@ import collections.abc
 
 import more_itertools as mitt
 
+T = t.TypeVar("T")
 
-class FOSet(collections.abc.Set, collections.abc.Hashable, collections.abc.Collection):
+
+class FOSet(
+    collections.abc.Set[T],
+    collections.abc.Hashable,
+    collections.abc.Collection[T],
+):
     """
     Frozen Ordered Set. Immutable, set-like container that retains the order of
     elements. Hashable.
     """
 
-    def __init__(self, iterable: t.Iterable):
+    def __init__(self, iterable: t.Iterable[T]):
         ordered = list(mitt.unique_everseen(iterable))
 
         self._set = frozenset(ordered)
         self._list = ordered
 
     # -------- Set --------
-    def __contains__(self, x):
+    def __contains__(self, x: T):
         return x in self._set
 
     def __iter__(self):
